91f4fc57fa2d29b41c4af3f02d412c2b026b53c4
MonstrHW <nukolau08@gmail.com>
2021-11-30 15:21:23 +0800

    Add scripts for starting tool

---
+++ start.bat
@@ -0,0 +1,4 @@
+Rem *** Example ***
+Rem start "" ".\PoeTradeNotifier.exe" -p "C:\Program Files (x86)\Steam\steamapps\common\Path of Exile\logs\Client.txt" -t "2100892913:BCSGwgfeBCgHai1Qv_XUwZnWRKoDt0-lKTc" -c 431963451 -a
+
+start "" ".\PoeTradeNotifier.exe" -p "" -t "" -c 0 -a
